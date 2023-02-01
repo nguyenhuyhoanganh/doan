522e3cfbf8a5b0e4bcdfc4e689fce8b693827a78
nguyenhuyhoanganh <77695890+nguyenhuyhoanganh@users.noreply.github.com>
2023-02-01 21:51:05 +0700

    BE/ fix

--- FE/admin/appmusic/src/utils/validate.form.js
+++ FE/admin/appmusic/src/utils/validate.form.js
@@ -1,57 +1,5 @@
 import * as yup from 'yup'
 
-export const getRules = (getValues = undefined) => ({
-  email: {
-    required: {
-      value: true,
-      message: 'Email is required'
-    },
-    pattern: {
-      value: /^\S+@\S+\.\S+$/,
-      message: 'Email is not valid'
-    },
-    maxLength: {
-      value: 160,
-      message: 'Email length from 5 - 160 characters'
-    },
-    minLength: {
-      value: 5,
-      message: 'Email length from 5 - 160 characters'
-    }
-  },
-  password: {
-    required: {
-      value: true,
-      message: 'Password is required'
-    },
-    maxLength: {
-      value: 160,
-      message: 'Password length from 6 - 160 characters'
-    },
-    minLength: {
-      value: 6,
-      message: 'Password length from 6 - 160 characters'
-    }
-  },
-  confirm_password: {
-    required: {
-      value: true,
-      message: 'Confirm password is required'
-    },
-    maxLength: {
-      value: 160,
-      message: 'Confirm password length from 6 - 160 characters'
-    },
-    minLength: {
-      value: 6,
-      message: 'Confirm password length from 6 - 160 characters'
-    },
-    validate: getValues
-      ? (value) => value === getValues('password') || "Confirm password doesn't match password"
-      : undefined
-  }
-})
-
 export const registerSchema = yup.object({
   email: yup
     .string()
@@ -71,3 +19,55 @@ export const registerSchema = yup.object({
     .max(160, 'Confirm password length from 6 - 160 characters')
     .oneOf([yup.ref('password')], "Confirm password doesn't match password")
 })
+
+// export const getRules = (getValues = undefined) => ({
+//   email: {
+//     required: {
+//       value: true,
+//       message: 'Email is required'
+//     },
+//     pattern: {
+//       value: /^\S+@\S+\.\S+$/,
+//       message: 'Email is not valid'
+//     },
+//     maxLength: {
+//       value: 160,
+//       message: 'Email length from 5 - 160 characters'
+//     },
+//     minLength: {
+//       value: 5,
+//       message: 'Email length from 5 - 160 characters'
+//     }
+//   },
+//   password: {
+//     required: {
+//       value: true,
+//       message: 'Password is required'
+//     },
+//     maxLength: {
+//       value: 160,
+//       message: 'Password length from 6 - 160 characters'
+//     },
+//     minLength: {
+//       value: 6,
+//       message: 'Password length from 6 - 160 characters'
+//     }
+//   },
+//   confirm_password: {
+//     required: {
+//       value: true,
+//       message: 'Confirm password is required'
+//     },
+//     maxLength: {
+//       value: 160,
+//       message: 'Confirm password length from 6 - 160 characters'
+//     },
+//     minLength: {
+//       value: 6,
+//       message: 'Confirm password length from 6 - 160 characters'
+//     },
+//     validate: getValues
+//       ? (value) => value === getValues('password') || "Confirm password doesn't match password"
+//       : undefined
+//   }
+// })
